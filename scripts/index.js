663000a31143799fd481e05184de3b99d43f85fe
Mohammed ayman <mae252002@gmail.com>
2021-07-04 21:15:11 +0200

    activating the task deletion button

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,56 @@
 import {paginate,showPage, removePaginationBtns} from './pagination.js';
 
 // CONSTANTS ::
-const elementsPerPage = 6;
-const tableId = 'todo-content-table';
+const elementsPerPage = 4;
 const paginationUlId = 'todo-pagination-ul';
+const todoItemSelector = '.todo-item';
 const todoInput = document.getElementById('todo-input');
 const todoBtn = document.getElementById('todo-btn');
-const contentTable = document.getElementById(tableId).getElementsByTagName('tbody')[0];
+const todoList = document.getElementsByClassName('todo-list')[0];
+const trashBtns = document.getElementsByClassName('trash-btn');
 
 // INITIATE ::
-paginate(tableId,elementsPerPage);
-showPage('1',tableId);
+paginate(todoItemSelector,elementsPerPage);
+showPage('1',todoItemSelector);
 
 // EVENTS ::
-todoBtn.addEventListener('click', addTask)
+todoBtn.addEventListener('click', addTask);
+
+// FUNCTIONS ::
+
+function emptyTodoInput() {
+  todoInput.value = '';
+}
 
 function addTask() {
   if (todoInput.value.trim().length > 0) {
-    const tableTr = document.createElement('tr');
-    const firstTd = document.createElement('td');
-    const secondTd = document.createElement('td');
-    const taskDeleteBtn = document.createElement('button');
-    taskDeleteBtn.name = 'task-delete';
-    taskDeleteBtn.innerHTML = '<i class="fa fa-remove"></i>';
-    firstTd.innerHTML = todoInput.value.trim();
-    secondTd.appendChild(taskDeleteBtn);
-    tableTr.appendChild(firstTd);
-    tableTr.appendChild(secondTd);
-    contentTable.insertBefore(tableTr, contentTable.childNodes[0]);
-    paginate(tableId,elementsPerPage);
-    showPage('1',tableId);
+    const todoItemDiv = document.createElement('div');
+    todoItemDiv.setAttribute('class', 'todo-item');
+    const todoItemLi = document.createElement('li');
+    todoItemLi.innerHTML = todoInput.value.trim();
+    const todoItemCheckBtn = document.createElement('button');
+    todoItemCheckBtn.setAttribute('class','check-btn');
+    todoItemCheckBtn.innerHTML = '<li class="fa fa-check"></li>';
+    const todoItemTrashBtn = document.createElement('button');
+    todoItemTrashBtn.setAttribute('class','trash-btn');
+    todoItemTrashBtn.innerHTML = '<li class="fa fa-trash"></li>';
+    todoItemTrashBtn.addEventListener('click', function() {
+      deleteTask(event);
+    });
+    todoItemDiv.appendChild(todoItemLi);
+    todoItemDiv.appendChild(todoItemCheckBtn);
+    todoItemDiv.appendChild(todoItemTrashBtn);
+    todoList.insertBefore(todoItemDiv, todoList.childNodes[0]);
+    paginate(todoItemSelector,elementsPerPage);
+    showPage('1',todoItemSelector);
+    emptyTodoInput();
   } else{
     return;
   }
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove();
+  paginate(todoItemSelector,elementsPerPage);
+  showPage('1',todoItemSelector);
 }
--- scripts/pagination.js
+++ scripts/pagination.js
@@ -6,18 +6,17 @@ export const removePaginationBtns = () => {
   }
 }
 
-
 // REMOVE ALL PAGES ::
-export const removePages = (tableId) => {
-  let rows = document.querySelectorAll(`#${tableId} > tbody > tr`);
+export const removePages = (todoItemSelector) => {
+  let rows = document.querySelectorAll(`${todoItemSelector}`);
   for (var i = 0; i < rows.length; i++) {
     rows[i].style.display = 'none';
   }
 }
 
 // SHOW A SPECIFIC PAGE ::
-export const showPage = (pageId,tableId) => {
-  removePages(tableId);
+export const showPage = (pageId,todoItemSelector) => {
+  removePages(todoItemSelector);
   let pageRows = document.getElementsByClassName(`${pageId}`);
   for (var i = 0; i < pageRows.length; i++) {
     pageRows[i].style.display = '';
@@ -25,33 +24,33 @@ export const showPage = (pageId,tableId) => {
 }
 
 // ADD PAGINATION BUTTONS ::
-export const addPaginationBtn = (btnId) => {
+export const addPaginationBtn = (btnId, todoItemSelector) => {
   let paginationUl = document.getElementById('todo-pagination-ul');
   let newLi = document.createElement('li');
   let newLiBtn = document.createElement('button');
   newLiBtn.setAttribute('value',btnId);
   newLiBtn.innerHTML = btnId;
   newLiBtn.addEventListener("click", function() {
-    showPage(this.innerHTML,'todo-content-table');
+    showPage(this.innerHTML, todoItemSelector);
   });
   newLi.appendChild(newLiBtn);
   paginationUl.appendChild(newLi);
 }
 
 // THE GENERAL PAGINATION FUNCTION ::
-export const paginate = (tableId, elementsPerPage) =>{
+export const paginate = (todoItemSelector, elementsPerPage) =>{
   removePaginationBtns();
-  let rows = document.querySelectorAll(`#${tableId} > tbody > tr`);
+  let rows = document.querySelectorAll(`${todoItemSelector}`);
   let counter = 0;
   let id = 1;
   for (var i = 0; i < rows.length; i++) {
     if(counter === elementsPerPage){
-      addPaginationBtn(id);
+      addPaginationBtn(id, todoItemSelector);
       counter = 0;
       id += 1;
     }
-    rows[i].setAttribute('class',id);
+    rows[i].className = `todo-item ${id}`;
     counter += 1;
   }
-  (counter !=0 && id != 1) ? addPaginationBtn(id) : False;
+  (counter !=0 && id != 1) ? addPaginationBtn(id, todoItemSelector) : 0;
 }
